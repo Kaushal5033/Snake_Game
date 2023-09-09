d9b1e7cb234270fbc203870d74eccec8a4ee0042
Kaushal5033 <kaushalrai8076958922@gmail.com>
2023-09-09 11:41:02 +0530

    1st

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let direction = {x: 0 , y : 0}
+foodsound = new Audio('foodsound.mp3');
+gameoversound = new Audio('gameover.mp3');
+movesound = new Audio('move.mp3');
+musicsound = new Audio('gamemusic.mp3');
